a1cde0f8b7515dd93c5490a2335e3dd7880799b1
Jennifer Renoux <jrx-e@edunet.oru.se>
2022-01-07 15:08:10 +0100

    separated launcher from rest of gui

---
+++ protocol/intra-group-protocol.py
@@ -0,0 +1,4 @@
+"""Main class implementing the user-study protocol. The protocol is as follows:
+1. Each participant is assigned to one of the conditions (balanced)
+2. Each participant plays the game as many times as required by their condition (balanced accross agents)
+"""
